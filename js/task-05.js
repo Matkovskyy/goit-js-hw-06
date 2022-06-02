91af23a0ebd505774b4c72877320e2a13242a274
Matkovskyy <vadym.matkovskyy@gmail.com>
2022-06-02 12:10:18 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,15 @@
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
-inputEl.addEventListener('input',inputChange)
-function inputChange(event){
- outputEl.textContent = event.currentTarget.value
-}
-function reloadInput(event) {
-    
-    if (event.currentTarget.value === '') {
-        outputEl.textContent = 'Anonymous'
-    }
-}
- console.log(inputEl.addEventListener('blur', reloadInput))
+const inputChange = () => {
+	const DEFAULT_NAME = 'Anonymous';
+	const { value } = inputEl;
+
+	if (value) { outputEl.textContent = value }
+    else { outputEl.textContent = DEFAULT_NAME }       
+        	
+};
+
+inputEl.addEventListener('input', inputChange);
   
    
